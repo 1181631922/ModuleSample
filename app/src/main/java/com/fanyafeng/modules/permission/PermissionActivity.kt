9d5f6070f47ae8ca5bd3f0ff9d19d7cd68ec8a0b
fanyafeng <fanyafeng@live.cn>
2020-04-13 15:45:48 +0800

    添加动态权限测试库

---
+++ app/src/main/java/com/fanyafeng/modules/permission/PermissionActivity.kt
@@ -0,0 +1,26 @@
+package com.fanyafeng.modules.permission
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fanyafeng.modules.R
+
+/**
+ * 动态权限测试activity
+ */
+class PermissionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_permission)
+        initView()
+        initData()
+    }
+
+    private fun initView(){
+
+    }
+
+    private fun initData(){
+
+    }
+}
