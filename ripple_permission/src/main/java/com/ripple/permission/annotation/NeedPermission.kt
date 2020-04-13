7c0eb12f22b371d478c68b14180ae6ab5a5b8b21
fanyafeng <fanyafeng@live.cn>
2020-04-13 18:18:04 +0800

    进行gradle脚本插桩权限

---
+++ ripple_permission/src/main/java/com/ripple/permission/annotation/NeedPermission.kt
@@ -0,0 +1,13 @@
+package com.ripple.permission.annotation
+
+/**
+ * Author: fanyafeng
+ * Data: 2020/4/13 18:09
+ * Email: fanyafeng@live.cn
+ * Description:
+ */
+
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.BINARY)
+annotation class NeedPermission {
+}
