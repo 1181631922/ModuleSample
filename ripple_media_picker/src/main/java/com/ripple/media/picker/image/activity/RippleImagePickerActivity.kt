b0a249467d61c3ede09a589f6402dff58d73affa
fanyafeng <fanyafeng@live.cn>
2020-04-22 16:03:54 +0800

    解决glide加载图片问题

--- ripple_media_picker/src/main/java/com/ripple/media/picker/image/activity/RippleImagePickerActivity.kt
+++ ripple_media_picker/src/main/java/com/ripple/media/picker/image/activity/RippleImagePickerActivity.kt
@@ -1,23 +1,29 @@
 package com.ripple.media.picker.image.activity
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
-import androidx.appcompat.widget.Toolbar
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.GridLayoutManager
 import com.ripple.media.picker.R
 import com.ripple.media.picker.base.RippleBaseActivity
 import com.ripple.media.picker.image.ScanImageSource
 import com.ripple.media.picker.image.adapter.RippleImageAdapter
 import com.ripple.media.picker.model.RippleFolderModel
-import com.ripple.media.picker.model.RippleImageModel
 import com.ripple.media.picker.model.RippleMediaModel
 import com.ripple.media.picker.util.LogUtil
 import kotlinx.android.synthetic.main.activity_ripple_image_picker.*
+
 
 class RippleImagePickerActivity : RippleBaseActivity(), ScanImageSource.ImageSourceListener {
 
     companion object {
         private val TAG = RippleImagePickerActivity.javaClass.simpleName
         private val LINE = 4
+        val REQUEST_CODE_WRITE_EXTERNAL_STORAGE = 101
     }
 
     private lateinit var scanImageSource: ScanImageSource
@@ -28,6 +34,32 @@ class RippleImagePickerActivity : RippleBaseActivity(), ScanImageSource.ImageSou
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ripple_image_picker)
 
+//        initView()
+//        initData()
+        requestMediaPermission()
+    }
+
+    private fun requestMediaPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ) !== PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf<String>(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                    REQUEST_CODE_WRITE_EXTERNAL_STORAGE
+                )
+            } else {
+                operateData()
+            }
+        } else {
+            operateData()
+        }
+    }
+
+    private fun operateData() {
         initView()
         initData()
     }
@@ -39,7 +71,7 @@ class RippleImagePickerActivity : RippleBaseActivity(), ScanImageSource.ImageSou
     }
 
     private fun initData() {
-        adapter = RippleImageAdapter(this, list, LINE)
+
     }
 
     /**
@@ -49,7 +81,30 @@ class RippleImagePickerActivity : RippleBaseActivity(), ScanImageSource.ImageSou
         LogUtil.d(TAG, mediaList.toString())
         if (mediaList.isNotEmpty()) {
             list.addAll(mediaList[0].getMediaList())
+            adapter = RippleImageAdapter(this, list, LINE)
             rippleImageRV.adapter = adapter
+            adapter?.notifyDataSetChanged()
         }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            REQUEST_CODE_WRITE_EXTERNAL_STORAGE -> if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                operateData()
+            } else {
+                Toast.makeText(
+                    this@RippleImagePickerActivity,
+                    "Permission Denied",
+                    Toast.LENGTH_SHORT
+                )
+                    .show()
+                finish()
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 }
---
+++ ripple_media_picker/src/main/java/com/ripple/media/picker/provider/RippleMediaPickProvider.kt
@@ -0,0 +1,11 @@
+package com.ripple.media.picker.provider
+
+import androidx.core.content.FileProvider
+
+/**
+ * Author: fanyafeng
+ * Data: 2020/4/22 15:27
+ * Email: fanyafeng@live.cn
+ * Description:
+ */
+class RippleMediaPickProvider : FileProvider()
