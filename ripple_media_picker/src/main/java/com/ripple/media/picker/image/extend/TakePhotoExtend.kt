b817110bbbd4b79571bc71f1a9314679c56adb41
fanyafeng <fanyafeng@live.cn>
2020-04-30 16:36:06 +0800

    拍照和选图完成kt封装

--- ripple_media_picker/src/main/java/com/ripple/media/picker/image/extend/TakePhotoExtend.kt
+++ ripple_media_picker/src/main/java/com/ripple/media/picker/image/extend/TakePhotoExtend.kt
@@ -1,5 +1,6 @@
 package com.ripple.media.picker.image.extend
 
+import android.app.Activity
 import android.content.Context
 import com.ripple.media.picker.image.RippleImagePick
 import com.ripple.media.picker.image.listener.SelectImageListListener
@@ -14,11 +15,15 @@ import java.io.File
  * Email: fanyafeng@live.cn
  * Description:
  */
-fun Context.takePhoto( successLambda: SuccessLambda<File?>) {
+fun Context.takePhoto(successLambda: SuccessLambda<File?>) {
     RippleImagePick.getInstance().takePhotoListener = object : TakePhotoListener {
         override fun onTakePhotoListener(file: File?) {
             successLambda?.invoke(file)
         }
     }
     RippleImagePick.getInstance().takePhoto(this)
+}
+
+fun Activity.takePhoto(successLambda: SuccessLambda<File?>) {
+    (this as Context).takePhoto(successLambda)
 }
