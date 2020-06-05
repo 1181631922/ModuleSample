d26126728b2775c820c94f6baa0059eba1594534
fanyafeng <fanyafeng@live.cn>
2020-06-05 15:20:04 +0800

    Author: fanyafeng Email: fanyafeng@live.cn Modify: 1.发布task库到jcenter

--- ripple_task/src/main/java/com/ripple/task/engine/ProcessEngine.kt
+++ ripple_task/src/main/java/com/ripple/task/engine/ProcessEngine.kt
@@ -3,6 +3,7 @@ package com.ripple.task.engine
 import java.io.Serializable
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
+import java.util.concurrent.ScheduledExecutorService
 
 /**
  * Author: fanyafeng
@@ -42,6 +43,30 @@ interface ProcessEngine : Serializable {
 
     /**
      * 获取任务处理任务
+     *
+     * 1、线程池： 提供一个线程队列，队列中保存着所有等待状态的线程。避免了创建与销毁的额外开销，提高了响应的速度。
+     *
+     * [java.util.concurrent.Executor]
+     * [java.util.concurrent.ExecutorService]
+     * [java.util.concurrent.ThreadPoolExecutor]
+     * [java.util.concurrent.ScheduledExecutorService]
+     * [java.util.concurrent.ScheduledThreadPoolExecutor]
+     *
+     * 2、线程池的体系结构：
+     * java.util.concurrent.Executor 负责线程的使用和调度的根接口
+     *      |--ExecutorService 子接口： 线程池的主要接口
+     *              |--ThreadPoolExecutor 线程池的实现类
+     *              |--ScheduledExecutorService 子接口： 负责线程的调度
+     *                      |--ScheduledThreadPoolExecutor : 继承ThreadPoolExecutor，实现了ScheduledExecutorService
+     *
+     * [java.util.concurrent.Executor]
+     *
+     * 3、工具类 ： Executors
+     * ExecutorService newFixedThreadPool() : 创建固定大小的线程池
+     * ExecutorService newCachedThreadPool() : 缓存线程池，线程池的数量不固定，可以根据需求自动的更改数量。
+     * ExecutorService newSingleThreadExecutor() : 创建单个线程池。 线程池中只有一个线程
+     *
+     * ScheduledExecutorService newScheduledThreadPool() : 创建固定大小的线程，可以延迟或定时的执行任务
      */
     fun getExecutorService(): ExecutorService
 
