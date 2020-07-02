0c321f349a84b0b94b422156646ae3cf67e1b7f3
fanyafeng <fanyafeng@live.cn>
2020-07-02 18:43:20 +0800

    Author: fanyafeng Email: fanyafeng@live.cn Modify: 1.添加多标签动图

--- app/src/main/java/com/fanyafeng/modules/BaseActivity.kt
+++ app/src/main/java/com/fanyafeng/modules/BaseActivity.kt
@@ -3,6 +3,7 @@ package com.fanyafeng.modules
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.fanyafeng.modules.mediapick.config.RippleImageLoadFrameImpl
+import com.ripple.dialog.widget.impl.RippleToast
 import com.ripple.media.picker.RippleMediaPick
 import com.ripple.permission.RipplePermissionImpl
 
@@ -10,5 +11,9 @@ open class BaseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+    }
+
+    fun showToast(message: CharSequence) {
+        RippleToast.show(this, message)
     }
 }
--- app/src/main/java/com/fanyafeng/modules/flowlayout/FlowLayoutActivity.kt
+++ app/src/main/java/com/fanyafeng/modules/flowlayout/FlowLayoutActivity.kt
@@ -37,17 +37,20 @@ class FlowLayoutActivity : BaseActivity() {
             list.add(model)
             val itemView = ChooseItemView(this)
             itemView.setInnerTagWrapContent()
-            itemView.chooseViewUnselected=R.drawable.choose_view_normal
+            itemView.chooseViewUnselected = R.drawable.choose_view_normal
             chooseItemView.addItemView(itemView, model)
         }
 
 
         chooseItemView.onItemAbleClickListener = { view, position, model ->
             Log.d(TAG, "被点击：" + position)
+            showToast("被点击：" + position)
+
         }
 
         chooseItemView.onItemUnableClickListener = { view, position, model ->
             Log.d(TAG, "不能被点击的被点击了：" + position)
+            showToast("不能被点击的被点击了：" + position)
         }
 
         val itemWidth = (getScreenWidth() - 10.dp2px).oneQuadra
@@ -71,6 +74,7 @@ class FlowLayoutActivity : BaseActivity() {
         splitFlowView.onItemClickListener =
             { first: View?, position: Int?, third: IChooseModel?, fourth: Boolean?, fifth: Boolean? ->
                 println("被点击位置：" + position)
+                showToast("被点击位置：" + position)
             }
 
     }
@@ -78,7 +82,12 @@ class FlowLayoutActivity : BaseActivity() {
     private fun initData() {
         val viewList = mutableListOf<Pair<IChooseModel, ChooseItemView>>()
         (0 until 7).forEach { it ->
-            val model = ChooseModel("我是新增的第$it 个view", true, (it == 3 || it == 4))
+            val title = if (it < 5) {
+                "我是第$it 被更新"
+            } else {
+                "我是新增的第$it 个view"
+            }
+            val model = ChooseModel(title, true, (it == 3 || it == 4))
             val itemView = ChooseItemView(this)
 //            itemView.setInnerTagWrapContent()
             viewList.add(Pair(model, itemView))
@@ -90,6 +99,7 @@ class FlowLayoutActivity : BaseActivity() {
         btn2.setOnClickListener {
             Log.d(TAG, "最后结果：" + chooseItemView.getSelectedResult().toString())
             Log.d(TAG, "所有列表状态：" + chooseItemView.getAllDataList().toString())
+            showToast("最后结果：" + chooseItemView.getSelectedResult().toString())
         }
 
     }
